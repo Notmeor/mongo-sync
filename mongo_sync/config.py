97cd7bb65136566471ebb485a1781da6a09baa46
xiongjianqiang <xiongjianqiang@idwzx.com>
2018-11-21 19:26:55 +0800

    Add env variable for config path

--- mongo_sync/config.py
+++ mongo_sync/config.py
@@ -3,7 +3,9 @@
 import os
 import yaml
 
-config_path = os.path.join(os.path.dirname(__file__), 'config.yaml')
+
+config_path = os.getenv('MONGOSYNC_CONF') or\
+    os.path.join(os.path.dirname(__file__), 'config.yaml')
 
 with open(config_path, 'r') as f:
     conf = yaml.safe_load(f)
